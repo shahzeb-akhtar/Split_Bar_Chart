0aae8defe4a5b1ab71ffa59419a817861c5c0778
Shahzeb Akhtar <shahzeb.akhtar@gmail.com>
2020-04-18 15:58:54 -0700

    Add new data

---
+++ data/data.js
@@ -0,0 +1,146 @@
+var data = [
+ {
+   "Name": "USA",
+   "Value": 734546
+ },
+ {
+   "Name": "Spain",
+   "Value": 191726
+ },
+ {
+   "Name": "Italy",
+   "Value": 175925
+ },
+ {
+   "Name": "France",
+   "Value": 151793
+ },
+ {
+   "Name": "Germany",
+   "Value": 143160
+ },
+ {
+   "Name": "UK",
+   "Value": 114217
+ },
+ {
+   "Name": "China",
+   "Value": 82719
+ },
+ {
+   "Name": "Turkey",
+   "Value": 82329
+ },
+ {
+   "Name": "Iran",
+   "Value": 80868
+ },
+ {
+   "Name": "Belgium",
+   "Value": 37183
+ },
+ {
+   "Name": "Russia",
+   "Value": 36793
+ },
+ {
+   "Name": "Brazil",
+   "Value": 36599
+ },
+ {
+   "Name": "Canada",
+   "Value": 33218
+ },
+ {
+   "Name": "Netherlands",
+   "Value": 31589
+ },
+ {
+   "Name": "Switzerland",
+   "Value": 27404
+ },
+ {
+   "Name": "Portugal",
+   "Value": 19685
+ },
+ {
+   "Name": "India",
+   "Value": 15722
+ },
+ {
+   "Name": "Ireland",
+   "Value": 14758
+ },
+ {
+   "Name": "Austria",
+   "Value": 14671
+ },
+ {
+   "Name": "Peru",
+   "Value": 14420
+ },
+ {
+   "Name": "Sweden",
+   "Value": 13822
+ },
+ {
+   "Name": "Israel",
+   "Value": 13265
+ },
+ {
+   "Name": "S. Korea",
+   "Value": 10653
+ },
+ {
+   "Name": "Japan",
+   "Value": 9787
+ },
+ {
+   "Name": "Chile",
+   "Value": 9730
+ },
+ {
+   "Name": "Ecuador",
+   "Value": 9022
+ },
+ {
+   "Name": "Poland",
+   "Value": 8742
+ },
+ {
+   "Name": "Romania",
+   "Value": 8418
+ },
+ {
+   "Name": "Saudi Arabia",
+   "Value": 8274
+ },
+ {
+   "Name": "Pakistan",
+   "Value": 7638
+ },
+ {
+   "Name": "Denmark",
+   "Value": 7242
+ },
+ {
+   "Name": "Norway",
+   "Value": 7036
+ },
+ {
+   "Name": "Mexico",
+   "Value": 6875
+ },
+ {
+   "Name": "Czechia",
+   "Value": 6606
+ },
+ {
+   "Name": "Australia",
+   "Value": 6565
+ },
+ {
+   "Name": "UAE",
+   "Value": 6302
+ }
+];
