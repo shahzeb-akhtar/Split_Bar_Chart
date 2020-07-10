e9c6c07a2d473ac06f2bd43e9d81fb2bdd69038c
shahzeb-akhtar <shahzeb.akhtar@gmail.com>
2020-07-09 20:31:08 -0700

    changed data, removed unused data files, added screen shot

--- data/data.js
+++ data/data.js
@@ -1,146 +1,162 @@
 var data = [
- {
-   "Name": "USA",
-   "Value": 734546
- },
- {
-   "Name": "Spain",
-   "Value": 191726
- },
- {
-   "Name": "Italy",
-   "Value": 175925
- },
- {
-   "Name": "France",
-   "Value": 151793
- },
- {
-   "Name": "Germany",
-   "Value": 143160
- },
- {
-   "Name": "UK",
-   "Value": 114217
- },
- {
-   "Name": "China",
-   "Value": 82719
- },
- {
-   "Name": "Turkey",
-   "Value": 82329
- },
- {
-   "Name": "Iran",
-   "Value": 80868
- },
- {
-   "Name": "Belgium",
-   "Value": 37183
- },
- {
-   "Name": "Russia",
-   "Value": 36793
- },
- {
-   "Name": "Brazil",
-   "Value": 36599
- },
- {
-   "Name": "Canada",
-   "Value": 33218
- },
- {
-   "Name": "Netherlands",
-   "Value": 31589
- },
- {
-   "Name": "Switzerland",
-   "Value": 27404
- },
- {
-   "Name": "Portugal",
-   "Value": 19685
- },
- {
-   "Name": "India",
-   "Value": 15722
- },
- {
-   "Name": "Ireland",
-   "Value": 14758
- },
- {
-   "Name": "Austria",
-   "Value": 14671
- },
- {
-   "Name": "Peru",
-   "Value": 14420
- },
- {
-   "Name": "Sweden",
-   "Value": 13822
- },
- {
-   "Name": "Israel",
-   "Value": 13265
- },
- {
-   "Name": "S. Korea",
-   "Value": 10653
- },
- {
-   "Name": "Japan",
-   "Value": 9787
- },
- {
-   "Name": "Chile",
-   "Value": 9730
- },
- {
-   "Name": "Ecuador",
-   "Value": 9022
- },
- {
-   "Name": "Poland",
-   "Value": 8742
- },
- {
-   "Name": "Romania",
-   "Value": 8418
- },
- {
-   "Name": "Saudi Arabia",
-   "Value": 8274
- },
- {
-   "Name": "Pakistan",
-   "Value": 7638
- },
- {
-   "Name": "Denmark",
-   "Value": 7242
- },
- {
-   "Name": "Norway",
-   "Value": 7036
- },
- {
-   "Name": "Mexico",
-   "Value": 6875
- },
- {
-   "Name": "Czechia",
-   "Value": 6606
- },
- {
-   "Name": "Australia",
-   "Value": 6565
- },
- {
-   "Name": "UAE",
-   "Value": 6302
- }
+  {
+    "Name": "Russia",
+    "Value": 17098246
+  },
+  {
+    "Name": "Canada",
+    "Value": 9984670
+  },
+  {
+    "Name": "China",
+    "Value": 9596961
+  },
+  {
+    "Name": "United States",
+    "Value": 9525067
+  },
+  {
+    "Name": "Brazil",
+    "Value": 8515767
+  },
+  {
+    "Name": "Australia",
+    "Value": 7692024
+  },
+  {
+    "Name": "India",
+    "Value": 3287263
+  },
+  {
+    "Name": "Argentina",
+    "Value": 2780400
+  },
+  {
+    "Name": "Kazakhstan",
+    "Value": 2724900
+  },
+  {
+    "Name": "Algeria",
+    "Value": 2381741
+  },
+  {
+    "Name": "Republic of the Congo",
+    "Value": 2344858
+  },
+  {
+    "Name": "Kingdom of Denmark",
+    "Value": 2220093
+  },
+  {
+    "Name": "Greenland (Denmark)",
+    "Value": 2166086
+  },
+  {
+    "Name": "Saudi Arabia",
+    "Value": 2149690
+  },
+  {
+    "Name": "Mexico",
+    "Value": 1964375
+  },
+  {
+    "Name": "Indonesia",
+    "Value": 1910931
+  },
+  {
+    "Name": "Sudan",
+    "Value": 1861484
+  },
+  {
+    "Name": "Libya",
+    "Value": 1759540
+  },
+  {
+    "Name": "Iran",
+    "Value": 1648195
+  },
+  {
+    "Name": "Mongolia",
+    "Value": 1564110
+  },
+  {
+    "Name": "Peru",
+    "Value": 1285216
+  },
+  {
+    "Name": "Chad",
+    "Value": 1284000
+  },
+  {
+    "Name": "Niger",
+    "Value": 1267000
+  },
+  {
+    "Name": "Angola",
+    "Value": 1246700
+  },
+  {
+    "Name": "Mali",
+    "Value": 1240192
+  },
+  {
+    "Name": "South Africa",
+    "Value": 1221037
+  },
+  {
+    "Name": "Colombia",
+    "Value": 1141748
+  },
+  {
+    "Name": "Ethiopia",
+    "Value": 1104300
+  },
+  {
+    "Name": "Bolivia",
+    "Value": 1098581
+  },
+  {
+    "Name": "Mauritania",
+    "Value": 1030700
+  },
+  {
+    "Name": "Egypt",
+    "Value": 1002450
+  },
+  {
+    "Name": "Tanzania",
+    "Value": 945087
+  },
+  {
+    "Name": "Nigeria",
+    "Value": 923768
+  },
+  {
+    "Name": "Venezuela",
+    "Value": 916445
+  },
+  {
+    "Name": "Pakistan",
+    "Value": 907132
+  },
+  {
+    "Name": "Namibia",
+    "Value": 825615
+  },
+  {
+    "Name": "Mozambique",
+    "Value": 801590
+  },
+  {
+    "Name": "Turkey",
+    "Value": 783562
+  },
+  {
+    "Name": "Chile",
+    "Value": 756102
+  },
+  {
+    "Name": "Zambia",
+    "Value": 752612
+  }
 ];
